806cbd0ccb85f9c9a410cdf7b1a747d313e7cd59
Arthur Fagundes Fetzner <68615679+Arthur-Fetzner@users.noreply.github.com>
2022-12-02 18:39:00 -0300

    Main

--- index.php
+++ index.php
@@ -1,60 +1,93 @@
 <?php
-        session_start();
-            if(isset($_POST['acao'])){
-                $dbHost = 'localhost';
-                $dbUsername = 'root';
-                $dbPassword = '';
-                $dbName = 'autista';
+    session_start();
+    $extensao = '';
+    if(isset($_POST['acao'])){
+        $dbHost = 'localhost';
+        $dbUsername = 'root';
+        $dbPassword = '';
+        $dbName = 'autista';
     
-                $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+        $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
                 
-                $sql = 'Select Max(Postagem) from n_total';
-                $result = $conexao->query($sql);
-                $result = mysqli_fetch_assoc($result);
-                $new = $result['Max(Postagem)'] + 1;
+        $sql = 'Select Max(Postagem) from n_total';
+        $result = $conexao->query($sql);
+        $result = mysqli_fetch_assoc($result);
+        $new = $result['Max(Postagem)'] + 1;
 
-                if(empty($result['Max(Postagem)'])){
-                    $sql = 'INSERT INTO n_total VALUES (1)';
-                }else{
-                    $sql = "INSERT INTO n_total VALUES ($new)";
-                };
+        if(empty($result['Max(Postagem)'])){
+            $sql = 'INSERT INTO n_total VALUES (1)';
+        }else{
+            $sql = "INSERT INTO n_total VALUES ($new)";
+        };
 
-                $result = $conexao->query($sql);
+        $result = $conexao->query($sql);
+               
+        $arquivo = $_FILES['file'];
+        $extensao = explode('.',$arquivo['name'])[1];
                 
-                $arquivo = $_FILES['file'];
-                $extensao = explode('.',$arquivo['name'])[1];
-                
-                move_uploaded_file($arquivo['tmp_name'],'./arquivos/a fazer/'.$new.'.'.$extensao);
+        move_uploaded_file($arquivo['tmp_name'],'./arquivos/a fazer/'.$new.'.'.$extensao);
 
-                $_SESSION['id'] = $new;
-                $_SESSION['ext'] = $extensao;
-            };
-        ?>
+        $_SESSION['id'] = $new;
+        $_SESSION['ext'] = $extensao;
+    };
+?>
 
-<html>
-
-    <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-        <script>
+    <link rel="stylesheet" href="style-w-h.css">
+    <style>
+        .br-15{
+            border-radius: 15px;
+        }
+    </style>
+</head>
+<body class="p-4 d-flex" style="height: 100vh; background-color: #212121;">
+    <div class="w-100 d-flex align-items-center justify-content-center">
+        <div class="p-4 d-flex flex-column align-items-center border br-15 text-light" style="background-color: #616161; width: 400px;">
+            <h1 class="display-4">Spectrum</h1>
+            <small id="noticiar" class="text-center"></small>
+            <form action="" method="post" enctype="multipart/form-data" id="formulario" class="mt-4 mb-2">
+                <label for="file" class="btn br-15 text-light" style="background-color: #424242;">Selecionar imagem</label>
+                <input type="file" name="file" id="file" class="d-none" required>
+                <label for="acao" class="btn br-15 text-light" style="background-color: #424242;" onclick="verificar()" >Enviar</label>
+                <input type="submit" name="acao" id="acao" class="d-none">
+            </form>
+        </div>
+    </div>
+    <script>
 
-            setInterval(() => {
-                let id = "<?php if (isset($_SESSION['id'])){ echo $_SESSION['id'];};?>"
-                console.log(+id)
-                if (id != 0){
-                    window.location.href = "https://3db9-2804-14d-4c84-8e1d-2814-3eee-9d68-dc59.sa.ngrok.io/Spectrum/resultado.php"
-                }
-            }, 5000);
+        let id = "<?php if (isset($_SESSION['id'])){ echo $_SESSION['id'];};?>"
+        let extensao = "<?php if (isset($_SESSION['id'])){ echo $extensao;};?>"
+
+        if(extensao != 'png' && extensao != 'jpg' && extensao != 'jfif' && extensao != ''){
+            document.getElementById('noticiar').innerHTML = 'Formato de arquivo não suportado ou nome do arquivo contém um ponto'
+        }else{
+            if(id != 0){
+                document.getElementById('noticiar').innerHTML = 'Sua imagem está em processamento'
+                setTimeout(()=>{window.location.href = "http://localhost:80/Spectrum/resultado.php"},3000) 
+            }
+        }
+
+        function verificar(){
+            let arquivo = document.getElementById('file').files.length
         
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    </head>
+            if(arquivo == 0){
+                document.getElementById('noticiar').innerHTML = 'Nenhum arquivo selecionado' 
+            }else{
+                document.getElementById('acao').submit()
+            }
+        }
 
-    <body class="container d-flex align-items-center justify-content-center" style="background: #407070;">
-        <form action="" method="post" enctype="multipart/form-data" class="border d-flex flex-column p-4 w-50" style="border-radius: 15px; background: gray;">
-            <input type="file" name="file" class="mb-4" required>
-            <input type="submit" name="acao" value="Enviar">
-        </form>
-    </body>
+        
 
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body>
 </html>
--- resultado.php
+++ resultado.php
@@ -1,75 +1,84 @@
 <?php
-            session_start();
-            $arq = '';
-            $pred = 'vazio';
-            if (isset($_SESSION['id'])){
-                $arquivo = './arquivos/resultados/'.$_SESSION['id'].'.txt';
-                if (is_file($arquivo)){
-                    $arq = './arquivos/feitos/'.$_SESSION['id'].'.'.$_SESSION['ext'];
-                    $handle = fopen( $arquivo, 'r' );
-                    $ler = fread($handle, 1000);
-                    $pred = json_encode($ler);
-                    fclose($handle);
-                    session_destroy();
-                }else{
-				session_destroy();
-                    echo"Seu arquivo está em processamento, recarrege a pagina";
-                };
-            };
-        ?>
-
-<html>
-
-    <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    </head>
-
-    <body class="container d-flex align-items-center justify-content-center" style="background: #407070;">
-        <div class="border w-50 d-flex flex-column align-items-center justify-content-center p-4 text-white" style="border-radius: 15px; background: gray; gap:20px;">
-        <h3 id="escrever"></h3>
-        <canvas id="Canvas" class="w-100"></canvas>
-        <a href="https://3db9-2804-14d-4c84-8e1d-2814-3eee-9d68-dc59.sa.ngrok.io/Spectrum/" class="bg-white text-dark p-2 border">Voltar</a>
-        </div>
-    </body>
-    <script>
-        let arq = '<?php echo"$arq"; ?>';
-        if(arq != ''){
-            let canvas = document.getElementById('Canvas');
-            let ctx= canvas.getContext('2d');
-            let img = new Image();
-            img.src = arq;
-            img.onload=()=>{
-                canvas.width = img.width;
-                canvas.height = img.height;
-                ctx.drawImage(img, 0, 0);
-                //ctx.fillRect(0, 0, 100, 100);
-            };
-            
-            let pred = <?php echo $pred; ?>;
-            if (pred != 'vazio'){
-                pred = pred.replace(/["]/g, '');
-                console.log(pred);
-                let pred_array = pred.split("/");
-                for(let i = 0; i < pred_array.length; i++){
-                    pred_array[i] = pred_array[i].split(",");
-                };
-                img.onload=()=>{
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    ctx.drawImage(img, 0, 0);
-                    for(let i = 0; i < pred_array.length; i++){
-                        let j = pred_array[i];
-                        ctx.fillStyle = "red";
-                        ctx.lineWidth = 3;
-                        ctx.strokeRect(j[1], j[2], j[3], j[4]);
-                        ctx.font = "bold 2em Arial";
-                        ctx.fillText(j[0], j[1], j[2]-10);
-                    };
-                };
-                console.log(pred_array);
-            };
+    session_start();
+    $arq = '';
+    $pred = 'vazio';
+    if (isset($_SESSION['id'])){
+        $arquivo = './arquivos/resultados/'.$_SESSION['id'].'.txt';
+        if (is_file($arquivo)){
+            $arq = './arquivos/feitos/'.$_SESSION['id'].'.'.$_SESSION['ext'];
+            $handle = fopen( $arquivo, 'r' );
+            $ler = fread($handle, 1000);
+            $pred = json_encode($ler);
+            fclose($handle);
+            session_destroy();
+        }else{
+            echo"Seu arquivo está em processamento, recarrege a pagina";
         };
+    }else{
+        session_destroy();
+        header('Location: /Spectrum');
+    };
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="style-w-h.css">
+    <style>
+        .br-15{
+            border-radius: 15px;
+        }
+    </style>
+</head>
+<body class="p-4 d-flex" style="height: 100vh; background-color: #212121;">
+    <div class="w-100 d-flex align-items-center justify-content-center">
+        <div class="p-4 d-flex flex-column align-items-center border br-15 text-light" style="background-color: #616161; width:400px;">
+            <canvas id="Canvas" class="w-100 mb-4"></canvas>
+            <a href="http://localhost:80/Spectrum/unset.php" class="btn br-15 text-light" style="background-color: #424242;">Voltar</a>
+        </div>
+    </div>
+    <script>
+        let arq = '<?php echo $arq; ?>'
+        console.log(arq)
+        if(arq != ''){
+            let canvas = document.getElementById('Canvas')
+            let ctx= canvas.getContext('2d')
+            let img = new Image()
+            img.src = arq
+            let pred = '<?php echo $pred; ?>'
+            if(pred != 'vazio'){
+                pred = pred.replace(/["]/g, '')
+                let pred_array = pred.split("/")
+                for(let i = 0; i < pred_array.length; i++){
+                    pred_array[i] = pred_array[i].split(",")
+                }
+                img.onload=()=>{
+                    let tamanholetra = 2
+                    canvas.width = img.width
+                    canvas.height = img.height
+                    ctx.drawImage(img, 0, 0)
+
+                    if((img.width*img.height) > 500000){
+                        tamanholetra = 3
+                    }
+                    for(let i = 0; i < pred_array.length; i++){
+                        let j = pred_array[i]
+                        ctx.fillStyle = "red"
+                        ctx.lineWidth = 3
+                        ctx.strokeRect(j[1], j[2], j[3], j[4])
+                        ctx.font = `bold ${tamanholetra}em Arial`
+                        ctx.fillText(j[0], j[1], j[2]-10)
+                    }
+                }
+            }
+        }    
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body>
 </html>
